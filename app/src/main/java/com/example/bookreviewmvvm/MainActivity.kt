a22a3d4f1328467cf672c7094d1c021f9ab98459
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-17 01:05:34 +0900

    Add | Bestseller books' RecyclerView feature implementation

--- app/src/main/java/com/example/bookreviewmvvm/MainActivity.kt
+++ app/src/main/java/com/example/bookreviewmvvm/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.example.bookreviewmvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bookreviewmvvm.adapter.BookAdapter
+import com.example.bookreviewmvvm.api.BookService
+import com.example.bookreviewmvvm.api.Uri
+import com.example.bookreviewmvvm.databinding.ActivityMainBinding
+import com.example.bookreviewmvvm.model.BestSellerDto
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBookRecyclerView()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Uri.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(buildOkHttpClient())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSellerBooks(getString(R.string.INTERPARK_BOOKS))
+            .enqueue(object : Callback<BestSellerDto> {
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+
+                    if (response.isSuccessful.not()) {
+                        return
+                    }
+
+                    response.body()?.let { body ->
+                        adapter.submitList(body.books)
+                    }
+                }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+
+                }
+
+            })
+    }
+
+    private fun initBookRecyclerView() {
+        adapter = BookAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun buildOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if(BuildConfig.DEBUG) {
+                        HttpLoggingInterceptor.Level.BODY
+                    } else {
+                        HttpLoggingInterceptor.Level.NONE
+                    }
+                }
+            )
+            .build()
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
---
+++ app/src/main/java/com/example/bookreviewmvvm/adapter/BookAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.bookreviewmvvm.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookreviewmvvm.databinding.ItemBookBinding
+import com.example.bookreviewmvvm.model.Book
+
+class BookAdapter: ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
+
+    inner class BookItemViewHolder(private val binding: ItemBookBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(bookModel: Book) {
+            binding.titleTextView.text = bookModel.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
+        return BookItemViewHolder(ItemBookBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Book>() {
+            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
