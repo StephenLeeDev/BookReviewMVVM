54a66be9fd2675eb0bff89841b0db07c1c1fe49f
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-20 03:48:05 +0900

    Add | Searching history feature implementation

--- app/src/main/java/com/example/bookreviewmvvm/MainActivity.kt
+++ app/src/main/java/com/example/bookreviewmvvm/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.example.bookreviewmvvm.adapter.BookAdapter
+import com.example.bookreviewmvvm.adapter.HistoryAdapter
 import com.example.bookreviewmvvm.api.BookService
 import com.example.bookreviewmvvm.api.Uri
 import com.example.bookreviewmvvm.databinding.ActivityMainBinding
 import com.example.bookreviewmvvm.model.BestSellerDto
+import com.example.bookreviewmvvm.model.History
 import com.example.bookreviewmvvm.model.SearchBookDto
+import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -18,19 +23,35 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: BookAdapter
+    private lateinit var historyAdapter: HistoryAdapter
     private lateinit var bookService: BookService
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        job = Job()
+
         initBookRecyclerView()
+        initHistoryRecyclerView()
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "BookSearchDB"
+        ).build()
 
         val retrofit = Retrofit.Builder()
             .baseUrl(Uri.BASE_URL)
@@ -59,13 +80,8 @@ class MainActivity : AppCompatActivity() {
                 }
             })
 
-        binding.searchEditText.setOnKeyListener { view, keyCode, keyEvent ->
-            if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.action == MotionEvent.ACTION_DOWN) {
-                search(binding.searchEditText.text.toString())
-                return@setOnKeyListener true
-            }
-            return@setOnKeyListener false
-        }
+        initSearchEditText()
+
     }
 
     private fun search(keyword: String) {
@@ -76,6 +92,9 @@ class MainActivity : AppCompatActivity() {
                     response: Response<SearchBookDto>
                 ) {
 
+                    hideHistoryView()
+                    saveSearchKeyword(keyword)
+
                     if (response.isSuccessful.not()) {
                         return
                     }
@@ -85,6 +104,7 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onFailure(call: Call<SearchBookDto>, t: Throwable) {
 
+                    hideHistoryView()
                 }
             })
     }
@@ -93,6 +113,32 @@ class MainActivity : AppCompatActivity() {
         adapter = BookAdapter()
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
+    }
+
+    private fun initHistoryRecyclerView() {
+        historyAdapter = HistoryAdapter(historyDeleteClickListener = {
+            deleteSearchKeyword(it)
+        })
+        binding.historyRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.historyRecyclerView.adapter = historyAdapter
+        initSearchEditText()
+    }
+
+    private fun initSearchEditText() {
+        binding.searchEditText.setOnKeyListener { view, keyCode, keyEvent ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && keyEvent.action == MotionEvent.ACTION_DOWN) {
+                search(binding.searchEditText.text.toString())
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+
+        binding.searchEditText.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                showHistoryView()
+            }
+            return@setOnTouchListener false
+        }
     }
 
     private fun buildOkHttpClient(): OkHttpClient =
@@ -107,6 +153,41 @@ class MainActivity : AppCompatActivity() {
                 }
             )
             .build()
+
+    private fun showHistoryView() {
+        binding.historyRecyclerView.isVisible = true
+        launch(coroutineContext) {
+            withContext(Dispatchers.IO) {
+                val keywords = db.historyDao().getAll().reversed()
+
+                withContext(Dispatchers.Main) {
+                    binding.historyRecyclerView.isVisible = true
+                    historyAdapter.submitList(keywords.orEmpty())
+                }
+            }
+        }
+    }
+
+    private fun hideHistoryView() {
+        binding.historyRecyclerView.isVisible = false
+    }
+
+    private fun saveSearchKeyword(keyword: String) {
+        launch(coroutineContext) {
+            withContext(Dispatchers.IO) {
+                db.historyDao().insertHistory(History(null, keyword))
+            }
+        }
+    }
+
+    private fun deleteSearchKeyword(keyword: String) {
+        launch(coroutineContext) {
+            withContext(Dispatchers.IO) {
+                db.historyDao().delete(keyword)
+                showHistoryView()
+            }
+        }
+    }
 
     companion object {
         private const val TAG = "MainActivity"
