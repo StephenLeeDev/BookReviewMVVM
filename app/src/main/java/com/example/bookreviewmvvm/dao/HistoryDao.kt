19d7ec898ea513bcdc6bc1d2c6852a974bb3d061
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-20 03:43:27 +0900

    Add | generate searching history data model & data access object

---
+++ app/src/main/java/com/example/bookreviewmvvm/dao/HistoryDao.kt
@@ -0,0 +1,19 @@
+package com.example.bookreviewmvvm.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.bookreviewmvvm.model.History
+
+@Dao
+interface HistoryDao {
+
+    @Query("SELECT * FROM history")
+    fun getAll(): List<History>
+
+    @Insert
+    fun insertHistory(history: History)
+
+    @Query("DELETE FROM history WHERE keyword == :keyword")
+    fun delete(keyword: String)
+}
---
+++ app/src/main/java/com/example/bookreviewmvvm/model/History.kt
@@ -0,0 +1,11 @@
+package com.example.bookreviewmvvm.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class History(
+    @PrimaryKey val uid: Int?,
+    @ColumnInfo(name = "keyword") val keyword: String?
+)
