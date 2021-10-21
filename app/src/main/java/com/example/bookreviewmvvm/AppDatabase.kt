0ac525591e192f0f7e35debbc30985aa6e403660
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-21 10:03:16 +0900

    Add | Add review entity to RoomDatabase

--- app/src/main/java/com/example/bookreviewmvvm/AppDatabase.kt
+++ app/src/main/java/com/example/bookreviewmvvm/AppDatabase.kt
@@ -3,9 +3,12 @@ package com.example.bookreviewmvvm
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.bookreviewmvvm.dao.HistoryDao
+import com.example.bookreviewmvvm.dao.ReviewDao
 import com.example.bookreviewmvvm.model.History
+import com.example.bookreviewmvvm.model.Review
 
-@Database(entities = [History::class], version = 1)
+@Database(entities = [History::class, Review::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun historyDao(): HistoryDao
+    abstract fun reviewDao(): ReviewDao
 }
